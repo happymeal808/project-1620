5e554c786e57d7317fe2e2af89119e7549f44b19
Gill <gillianldowney12@gmail.com>
2025-03-27 18:03:17 -0700

    changin it

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+let fontHeader = document.getElementById("header");
+let fontSubHeader = document.getElementById("sub-header-font");
+let fontBody = document.getElementById("body-font");
+
+let color1 = document.getElementById("color-1");
+let color2 = document.getElementById("color-2");
+let color3 = document.getElementById("color-3");
+let color4 = document.getElementById("color-4");
+let color5 = document.getElementById("color5");
+
